5db11198539a90d5f3e341a69c209583072a68cf
xor9237 <ktlee927@gmail.com>
2020-05-11 00:58:41 -0700

    commit

--- scratch_1.py
+++ scratch_1.py
@@ -68,3 +68,50 @@ for lat, lng, borough, neighborhood in zip(df['latitude'], df['longitude'], df['
         fill_color='#3186cc',
         fill_opacity=0.7,
         parse_html=False).add_to(map_toronto)
+
+# Define Foursquare Credentials and Version
+CLIENT_ID = '3MAR2Y4AH5PUQKHSI4TCMUQMLU2S45MO0TVJAKMHDDHAGINK'  # Foursquare ID
+CLIENT_SECRET = 'SRPNGW0J3R5EFCSNVENLIXE4BUCCI0X2EEDBYRZTGYLY4HL3'  # Foursquare Secret
+VERSION = '20180605'  # Foursquare API version
+radius = 500
+LIMIT = 50
+
+
+def getNearbyVenues(names, latitudes, longitudes, radius=500):
+    venues_list = []
+    for name, lat, lng in zip(names, latitudes, longitudes):
+        print(name)
+
+        # create the API request URL
+        url = 'https://api.foursquare.com/v2/venues/explore?&client_id={}&client_secret={}&v={}&ll={},{}&radius={}&limit={}'.format(
+            CLIENT_ID,
+            CLIENT_SECRET,
+            VERSION,
+            lat,
+            lng,
+            radius,
+            LIMIT)
+
+        # make the GET request
+        results = requests.get(url).json()["response"]['groups'][0]['items']
+
+        # return only relevant information for each nearby venue
+        venues_list.append([(
+            name,
+            lat,
+            lng,
+            v['venue']['name'],
+            v['venue']['location']['lat'],
+            v['venue']['location']['lng'],
+            v['venue']['categories'][0]['name']) for v in results])
+
+    nearby_venues = pd.DataFrame([item for venue_list in venues_list for item in venue_list])
+    nearby_venues.columns = ['Neighborhood',
+                             'Neighborhood Latitude',
+                             'Neighborhood Longitude',
+                             'Venue',
+                             'Venue Latitude',
+                             'Venue Longitude',
+                             'Venue Category']
+
+    return (nearby_venues)
